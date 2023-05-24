600a2fdba571bd247ca8bcaf0b2ef6207902ac82
Rui Saraiva <ruisaraiva19@gmail.com>
2023-05-24 12:21:20 +0100

    feat: setup Tailwind CSS

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,8 +9,8 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div className="container mx-auto space-y-6 px-4">
+      <h1 className="text-3xl font-bold">Welcome to Remix</h1>
       <ul>
         <li>
           <a
